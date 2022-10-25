4850279aa68460c76d5a4c630740b79978f11850
mic-00 <94315060+mic-00@users.noreply.github.com>
2022-10-25 19:23:05 +0200

    js refactoring

--- controllers/header.php
+++ controllers/header.php
@@ -5,8 +5,11 @@ use Utils\UtilityFunctions;
 $user = $login = $logout = "";
 if (isset($_SESSION["username"], $_SESSION["email"], $_SESSION["password"], $_SESSION["admin"])) {
     $href = $_SESSION["admin"] ? "/area-amministratore/informazioni-personali" : "/area-utente/informazioni-personali";
-    $user = "<span id='user-welcome'>Ciao <a href='$href'>{$_SESSION["username"]}</a>!</span>";
+    $user = "<span id='user-welcome'>Ciao {$_SESSION["username"]}!</span>";
     $logout = "<li><a href='/logout' lang='en'>Logout</a></li>";
+    $logout = "<li><a href='/area-utente/informazioni-personali'>Area utente</a></li>" .
+        ($_SESSION["admin"] ? "<li><a href='/area-amministratore/informazioni-personali'>Area amministratore</a></li>" : "") .
+        $logout;
 } else {
     $login = "<li><a href='/login' lang='en'>Login</a></li>";
 }
--- public/index.js
+++ public/index.js
@@ -1,32 +1,36 @@
-window.onload = () => { document.getElementById('back-to-top').classList.add('hidden'); };
+window.onload = () => {
+  document.getElementById('back-to-top').classList.add('hidden');
 
-window.onscroll = function () {
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-    document.getElementById('back-to-top').classList.remove('hidden');
-  } else {
-    document.getElementById('back-to-top').classList.add('hidden');
-  }
-}
+  const inputIsValid = function (input, alert, validRegex) {
+    const cond = input.disabled || typeof validRegex === 'Array'
+        ? validRegex.every((regex) => input.value.match(regex))
+        : input.value.match(validRegex);
+    if (cond) {
+      alert.classList.add('hidden')
+      input.classList.remove('danger');
+      return true;
+    } else {
+      alert.classList.remove('hidden');
+      input.classList.add('danger');
+      return false;
+    }
+  };
 
-let f = [];
-
-const inputIsValid = function (input, alert, validRegex) {
-  if (input.value.match(validRegex)) {
-    alert.classList.add('hidden')
-    input.classList.remove('danger');
+  const formIsValid = function (inputs, alerts, validRegexes, event) {
+    let i = 0;
+    while (inputIsValid(inputs[i], alerts[i], validRegexes[i]) && i < inputs.length) i++;
+    if (i < inputs.length) {
+      event.preventDefault();
+      inputs[i].focus();
+      return false;
+    }
     return true;
-  } else {
-    alert.classList.remove('hidden');
-    input.classList.add('danger');
-    return false;
-  }
-}
+  };
 
-// HOME
-if (window.location.pathname === '/') {
+  // HOME
   let slideIndex = 1;
 
-  f['showSlides'] = function (n) {
+  const showSlides = function (n) {
     const slides = document.getElementsByClassName('slide');
     const dots = document.getElementsByClassName('dot');
     if (n > slides.length) { slideIndex = 1; }
@@ -41,185 +45,163 @@ if (window.location.pathname === '/') {
     dots[slideIndex-1].classList.add('active');
   };
 
-  f['plusSlides'] = function (n) {
-    f['showSlides'](slideIndex += n);
+  const plusSlides = function (n) {
+    showSlides(slideIndex += n);
   };
 
-  f['currentSlide'] = function (n) {
-    f['showSlides'](slideIndex = n);
+  const currentSlide = function (n) {
+    showSlides(slideIndex = n);
   };
 
-  f['showSlides'](slideIndex);
-}
-
-// REGISTRAZIONE
-// MODIFICA DATI UTENTE
-// MODIFICA DATI AMMINISTRATORE
-if (window.location.pathname === '/registrazione'
-    || window.location.pathname === '/area-utente/informazioni-personali/modifica'
-    || window.location.pathname === '/area-amministratore/informazioni-personali/modifica') {
-  f['formIsValid'] = function (event) {
-    if (!f['validateUsername']() || !f['validateEmail']() || !f['validatePwd']()) {
-      event.preventDefault();
-      if (!f['validateUsername']())
-        document.getElementById('username').focus();
-      else if (!f['validateEmail']())
-        document.getElementById('email').focus();
-      else
-        document.getElementById('password').focus();
-      return false;
+  if (window.location.pathname === '/') {
+    showSlides(slideIndex);
+    const prevs = document.getElementsByClassName('prev');
+    for (let i = 0; i < prevs.length; ++i) {
+      prevs[i].onclick = () => plusSlides(-1);
     }
-    return true;
-  }
-
-  f['validateUsername'] = function () {
-    const input = document.getElementById('username');
-    const alert = document.getElementById('username-alert');
-    const validRegex = /^(?=.{4,10}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9]+(?<![_.])$/;
-    return inputIsValid(input, alert, validRegex);
-  };
-
-  f['validateEmail'] = function () {
-    const input = document.getElementById('email');
-    const alert = document.getElementById('email-alert');
-    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    return inputIsValid(input, alert, validRegex);
-  };
-
-  f['validatePwd'] = function () {
-    const input = document.getElementById('password');
-    const alert = document.getElementById('pwd-alert');
-    const validRegex = [ /^\w{8,}$/, /[0-9]+/, /[A-Z]+/ ];
-    if (validRegex.every((regex) => input.value.match(regex))) {
-      alert.classList.add('hidden')
-      input.classList.remove('danger');
-      return true;
-    } else {
-      alert.classList.remove('hidden');
-      input.classList.add('danger');
-      return false;
+    const nexts = document.getElementsByClassName('next');
+    for (let i = 0; i < nexts.length; ++i) {
+      nexts[i].onclick = () => plusSlides(1);
+    }
+    const dots = document.getElementsByClassName('dot');
+    for (let i = 0; i < dots.length; ++i) {
+      dots[i].onclick = () => currentSlide(i + 1);
     }
   }
-}
 
-// ACQUISTO
-// RIPARAZIONE
-// GESTIONE MODELLI
-if (window.location.pathname === '/acquisto'
-    || window.location.pathname === '/riparazione'
-    || window.location.pathname === '/area-amministratore/gestione-modelli'
-    || window.location.pathname === '/area-amministratore/gestione-modelli/aggiungi'
-    || window.location.pathname === '/area-amministratore/aggiungi-articolo') {
-  fetch('/public/brands.php')
-      .then((res) => res.json())
-      .then(function (data) {
-        const selectBrand = document.getElementById('brand');
-        data.forEach(function (a) {
-          const option = document.createElement('option');
-          option.setAttribute('value', a);
-          const text = document.createTextNode(a);
-          option.appendChild(text);
-          selectBrand.appendChild(option);
-        });
-        if (data.length)
-          selectBrand.removeAttribute('disabled');
-      });
+  // REGISTRAZIONE
+  // MODIFICA DATI UTENTE
+  // MODIFICA DATI AMMINISTRATORE
+  if (window.location.pathname === '/registrazione'
+      || window.location.pathname === '/area-utente/informazioni-personali/modifica'
+      || window.location.pathname === '/area-amministratore/informazioni-personali/modifica') {
+    const username = document.getElementById("username");
+    const usernameAlert = document.getElementById('username-alert');
+    const usernameRegex = /^(?=.{4,10}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9]+(?<![_.])$/;
+    const email = document.getElementById('email');
+    const emailAlert = document.getElementById('email-alert');
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    const password = document.getElementById('password');
+    const passwordAlert = document.getElementById('pwd-alert');
+    const passwordRegex = [ /^\w{8,}$/, /[0-9]+/, /[A-Z]+/ ];
 
-  f['getModels'] = function () {
-    const selectBrand = document.getElementById('brand');
-    fetch(`/public/models.php?brand=${selectBrand.value}`)
+    username.onchange = () => inputIsValid(username, usernameAlert, usernameRegex);
+    email.onchange = () => inputIsValid(email, emailAlert, emailRegex);
+    password.onchange = () => inputIsValid(password, passwordAlert, passwordRegex);
+    document.getElementsByClassName('form')[0].onsubmit = (event) => {
+      const inputs = [ username, email, password ];
+      const alerts = [ usernameAlert, emailAlert, passwordAlert ];
+      const regex = [ usernameRegex, emailRegex, passwordRegex ];
+      return formIsValid(inputs, alerts, regex, event);
+    };
+  }
+
+  // ACQUISTO
+  // RIPARAZIONE
+  // GESTIONE MODELLI
+  if (window.location.pathname === '/acquisto'
+      || window.location.pathname === '/riparazione'
+      || window.location.pathname === '/area-amministratore/gestione-modelli'
+      || window.location.pathname === '/area-amministratore/gestione-modelli/aggiungi'
+      || window.location.pathname === '/area-amministratore/aggiungi-articolo') {
+    fetch('/public/brands.php')
         .then((res) => res.json())
         .then(function (data) {
-          const selectModel = document.getElementById('model');
-          while(selectModel.childElementCount > 1)
-            selectModel.removeChild(selectModel.lastChild);
+          const selectBrand = document.getElementById('brand');
           data.forEach(function (a) {
             const option = document.createElement('option');
             option.setAttribute('value', a);
             const text = document.createTextNode(a);
             option.appendChild(text);
-            selectModel.appendChild(option);
+            selectBrand.appendChild(option);
           });
           if (data.length)
-            selectModel.removeAttribute('disabled');
-          else
-            selectModel.setAttribute('disabled', 'disabled');
+            selectBrand.removeAttribute('disabled');
         });
+    document.getElementById('brand').onchange = (event) => {
+      fetch(`/public/models.php?brand=${event.target.value}`)
+          .then((res) => res.json())
+          .then(function (data) {
+            const selectModel = document.getElementById('model');
+            while(selectModel.childElementCount > 1)
+              selectModel.removeChild(selectModel.lastChild);
+            data.forEach(function (a) {
+              const option = document.createElement('option');
+              option.setAttribute('value', a);
+              const text = document.createTextNode(a);
+              option.appendChild(text);
+              selectModel.appendChild(option);
+            });
+            if (data.length)
+              selectModel.removeAttribute('disabled');
+            else
+              selectModel.setAttribute('disabled', 'disabled');
+          });
+    };
   }
-}
 
-// RIPARAZIONE
-if (window.location.pathname === '/riparazione') {
-  f['enableAlt'] = function () {
-    if (document.getElementById("image").files.length) {
-      document.getElementById("alt").removeAttribute("disabled");
+  // RIPARAZIONE
+  const enableAlt = () => {
+    if (document.getElementById('image').files.length) {
+      document.getElementById('alt').removeAttribute('disabled');
     } else {
-      document.getElementById("alt").setAttribute("disabled", "disabled");
-      document.getElementById("alt").classList.remove("danger");
-      document.getElementById("alt-alert").classList.add("hidden");
+      document.getElementById('alt').setAttribute('disabled', 'disabled');
+      document.getElementById('alt').classList.remove('danger');
+      document.getElementById('alt-alert').classList.add('hidden');
     }
   };
 
-  f['resetForm'] = function () {
-    const form = document.getElementById('repair-form');
-    form.reset();
-    f['enableAlt']();
+  const resetForm = () => {
+    document.getElementById('repair-form').reset();
+    const select = document.getElementById('model');
+    select.innerHTML = "<option value=''>--Seleziona--</option>";
+    enableAlt();
+  };
+
+  if (window.location.pathname === '/riparazione') {
+    const description = document.getElementById('description');
+    const descriptionAlert = document.getElementById('desc-alert');
+    const descriptionRegex = /\w+/;
+    const alt = document.getElementById('alt');
+    const altAlert = document.getElementById('alt-alert');
+    const altRegex = /\w+/;
+
+    description.onchange = () => inputIsValid(description, descriptionAlert, descriptionRegex);
+    alt.onchange = () => inputIsValid(alt, altAlert, altRegex);
+    document.getElementById('image').onchange = () => enableAlt()
+    document.getElementById('reset-button').onclick = () => resetForm();
+    document.getElementById('repair-form').onsubmit = (event) => {
+      const inputs = [ description, alt ];
+      const alert = [ descriptionAlert, altAlert ];
+      const regex = [ descriptionRegex, altRegex ];
+      return formIsValid(inputs, alert, regex, event);
+    };
   }
 
-  f['formIsValid'] = function (event) {
-    if (!f['validateDescription']() || !f['validateAlt']()) {
-      event.preventDefault();
-      if (!f['validateDescription']())
-        document.getElementById('description').focus();
-      else
-        document.getElementById('alt').focus();
-      return false;
+  // AREA AMMINISTRATORE -> GESTIONE RIPARAZIONI
+  if (window.location.pathname === '/area-amministratore/gestione-riparazioni') {
+    const cost = document.getElementById('cost');
+    const costAlert = document.getElementById('cost-alert');
+    const costRegex = /^[0-9]+.[0-9]{2}$|^[0-9]+$/;
+    const time = document.getElementById('time');
+    const timeAlert = document.getElementById('time-alert');
+    const timeRegex = /^[0-9]+$/;
+
+    cost.onchange = () => inputIsValid(cost, costAlert, costRegex);
+    time.onchange = () => inputIsValid(time, timeAlert, timeRegex);
+    document.getElementsByClassName('form')[0].onsubmit = (event) => {
+      const inputs = [ cost, time ];
+      const alerts = [ costAlert, timeAlert ];
+      const regexes = [ costRegex, timeRegex ];
+      return formIsValid(inputs, alerts, regexes, event);
     }
-    return true;
   }
+};
 
-  f['validateDescription'] = function () {
-    const input = document.getElementById('description');
-    const alert = document.getElementById('desc-alert');
-    const validRegex = /\w+/;
-    return inputIsValid(input, alert, validRegex);
-  }
-
-  f['validateAlt'] = function () {
-    const input = document.getElementById('alt');
-    if (input.disabled)
-      return true;
-    const alert = document.getElementById('alt-alert');
-    const validRegex = /\w+/;
-    return inputIsValid(input, alert, validRegex);
-  }
-}
-
-// AREA AMMINISTRATORE -> GESTIONE RIPARAZIONI
-if (window.location.pathname === '/area-amministratore/gestione-riparazioni') {
-  f['formIsValid'] = function (event) {
-    if (!f['validateCost']() || !f['validateTime']()) {
-      event.preventDefault();
-      if (!f['validateCost']())
-        document.getElementById('cost').focus();
-      else
-        document.getElementById('time').focus();
-      return false;
-    }
-    return true;
-  }
-
-  f['validateCost'] = function () {
-    const input = document.getElementById('cost');
-    const alert = document.getElementById('cost-alert');
-    const validRegex = /^[0-9]+.[0-9]{2}$|^[0-9]+$/;
-    return inputIsValid(input, alert, validRegex);
-  }
-
-  f['validateTime'] = function () {
-    const input = document.getElementById('time');
-    const alert = document.getElementById('time-alert');
-    const validRegex = /^[0-9]+$/;
-    return inputIsValid(input, alert, validRegex);
+window.onscroll = function () {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    document.getElementById('back-to-top').classList.remove('hidden');
+  } else {
+    document.getElementById('back-to-top').classList.add('hidden');
   }
 }
